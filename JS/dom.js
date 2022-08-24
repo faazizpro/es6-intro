adcba64fbc8b74ddf0860d8c401a8642f70e8a54
faazizpro <fahadafs11@gmail.com>
2022-08-24 15:51:46 +0600

    Recap of DOM Manipulation and event handler

--- JS/dom.js
+++ JS/dom.js
@@ -11,3 +11,16 @@ document.getElementById('center-third').addEventListener('click', function(){
     friend3.style.textAlign = 'center';
     friend3.style.textDecoration = 'underline'
 })
+
+
+document.getElementById('add-friend').addEventListener('click', function(){
+    const friendsContainer = document.getElementById('friends');
+
+    const friendDiv = document.createElement('div');
+    friendDiv.classList.add('friend');
+    friendDiv.innerHTML = `
+    <h3 class= "friend-name">New Friend Fahad</h3>
+    <p>Something New Friend Added</p>
+    `
+    friendsContainer.appendChild(friendDiv);
+})
