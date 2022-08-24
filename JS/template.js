a3f62632920ac70ddf79cc78182e40078650e396
faazizpro <fahadafs11@gmail.com>
2022-08-24 19:39:19 +0600

    Understand Template String

---
+++ JS/template.js
@@ -0,0 +1,22 @@
+//We can write a string in 3 ways
+const player1 = 'Adam';
+const player2 = "Ben White";
+const player3 = `Donald Trump`;
+
+const multiLine = 'First Line Text \n' +
+'Second Line text \n' +
+'Third Line Text \n' +
+'Fourth Line of Text \n';
+// console.log(multiLine); 
+
+const newMultiLine = `First Line Text 
+Second Line Text
+Third Line Text`;
+// console.log(newMultiLine);
+
+
+const a = 10;
+const b = 20;
+
+const newSummary = `sum ${a} and ${b} is equal to ${a+b}`;
+console.log(newSummary);
